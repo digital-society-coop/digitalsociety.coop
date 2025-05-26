298a653377e65cf9f05539e40556444038455b86
Endre Kadas <ekadas@gmail.com>
2025-05-26 16:35:18 +0100

    feat: Larger squint for section on big screens

--- components/Section.tsx
+++ components/Section.tsx
@@ -8,7 +8,7 @@ export default function Section(props: {
   return (
     <>
       {props.light === true && (
-        <div className="bg-linear-to-bl h-10 from-sumiInk1 from-49% to-lighterYellow to-50%" />
+        <div className="bg-linear-to-bl h-10 md:h-20 from-sumiInk1 from-49% to-lighterYellow to-50%" />
       )}
       <section
         className={`flex flex-col items-center justify-center ${props.light === true ? "bg-lighterYellow text-sumiInk1 [&_a]:text-linkBlue" : "[&_a]:text-lighterBlue"} ${props.className ?? ""} pb-8`}
@@ -16,7 +16,7 @@ export default function Section(props: {
         {props.children}
       </section>
       {props.light === true && (
-        <div className="bg-linear-to-br h-10 to-sumiInk1 from-49% from-lighterYellow to-50%" />
+        <div className="bg-linear-to-br h-10 md:h-20 to-sumiInk1 from-49% from-lighterYellow to-50%" />
       )}
     </>
   );

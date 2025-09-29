7a91cb04cb5e70fe31aaeb0c56b94d5d9ecae64d
Endre Kadas <endre@digitalsociety.coop>
2025-09-29 11:40:34 +0100

    feat: Add navigation menu

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,5 +1,12 @@
 import type { ReactNode } from "react";
 
-export default function Heading(props: { children: ReactNode }): ReactNode {
-  return <h2 className="text-xl sm:text-3xl my-6">{props.children}</h2>;
+export default function Heading(props: {
+  children: ReactNode;
+  anchor?: string;
+}): ReactNode {
+  return (
+    <h2 className="text-2xl sm:text-3xl my-6" id={props.anchor}>
+      {props.children}
+    </h2>
+  );
 }
--- components/Page.tsx
+++ components/Page.tsx
@@ -24,18 +24,117 @@ export default function Page(props: {
           src={webAnalyticsSrc}
           data-website-id={webAnalyticsId}
         ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          document.addEventListener('DOMContentLoaded', function() {
+            const menuToggle = document.getElementById('mobile-menu');
+
+            window.addEventListener('hashchange', function() {
+              menuToggle.checked = false;
+            });
+
+            const anchorLinks = document.querySelectorAll('a[href^="/#"]');
+            anchorLinks.forEach(link => {
+              link.addEventListener('click', function() {
+                menuToggle.checked = false;
+              });
+            });
+          });
+          `,
+          }}
+        />
       </head>
 
-      <body className="h-full margin-0 flex flex-col bg-sumiInk1 text-lighterYellow">
+      <body className="h-full margin-0 flex flex-col bg-sumiInk1 text-lighterYellow has-[#mobile-menu:checked]:overflow-hidden">
         <header>
-          <nav className="p-4">
-            <a href="/" aria-label="Back to the home page">
+          <nav className="flex flex-row justify-between p-4 shadow gap-1 w-full lg:z-1">
+            <a href="/" aria-label="Back to the home page" className="z-50">
               <img
                 className="h-[3rem] aspect-square block"
                 alt="Digital Society Ltd logo"
                 src="/images/logo-transparent-256x256.png"
               />
             </a>
+            <label
+              className="relative z-40 self-center p-2 sm:hidden text-lg"
+              htmlFor="mobile-menu"
+            >
+              <input
+                className="peer hidden"
+                type="checkbox"
+                id="mobile-menu"
+                defaultChecked={false}
+              />
+              <div className="relative z-50 block cursor-pointer">
+                <svg
+                  className="block size-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                  data-slot="icon"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+              </div>
+              <div className="fixed inset-0 z-40 h-full w-full bg-sumiInk3/50 backdrop-blur-sm hidden peer-checked:block">
+                &nbsp;
+              </div>
+              <div className="fixed top-[56px] right-0 z-40 w-full h-full -translate-y-full transition duration-300 hidden peer-checked:block peer-checked:translate-y-0">
+                <div className="float-top bg-sumiInk2 p-4 shadow-lg">
+                  <menu className="flex flex-col gap-6 items-center">
+                    <li>
+                      <Link href="/#projects" className="font-bold text-xl">
+                        Projects
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="/#products" className="font-bold text-xl">
+                        Products
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="/#team" className="font-bold text-xl">
+                        Team
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="/posts/" className="font-bold text-xl">
+                        Posts
+                      </Link>
+                    </li>
+                    <li>
+                      <a
+                        data-umami-event="out-mail"
+                        href="mailto:hello@digitalsociety.coop"
+                        className="font-bold text-xl self-center px-3 py-1 sm:mt-0 rounded-xl border border-linkBlue hover:outline outline-linkBlue bg-linkBlue! hover:bg-waveBlue2! text-lighterYellow!"
+                      >
+                        Get in touch {"\u2197"}
+                      </a>
+                    </li>
+                  </menu>
+                </div>
+              </div>
+            </label>
+            <div className="hidden sm:flex flex-row items-center gap-3 gap-2 md:gap-10 z-100">
+              <Link href="/#projects" className="font-bold">Projects</Link>
+              <Link href="/#products" className="font-bold">Products</Link>
+              <Link href="/#team" className="font-bold">Team</Link>
+              <Link href="/posts/" className="font-bold">Posts</Link>
+              <a
+                data-umami-event="out-mail"
+                href="mailto:hello@digitalsociety.coop"
+                className="font-bold self-center px-3 py-1 rounded-xl border border-linkBlue hover:outline outline-linkBlue bg-linkBlue! hover:bg-waveBlue2! text-lighterYellow!"
+              >
+                Get in touch {"\u2197"}
+              </a>
+            </div>
           </nav>
         </header>
         <main>{props.children}</main>

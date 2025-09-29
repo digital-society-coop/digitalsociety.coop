c6eb24ae150c706f444335bb4c16319da367d065
Endre Kadas <endre@digitalsociety.coop>
2025-09-29 17:57:44 +0100

    feat: Reworked home page to incorporate featured projects

--- content/index.11ty.tsx
+++ content/index.11ty.tsx
@@ -13,93 +13,84 @@ export default function Home(): ReactNode {
       title="Home"
       description="The home page for Digital Society, a not-for-profit cooperative helping you get your projects off the ground and realise the value of your data."
     >
-      <Section className="min-h-screen">
+      <Section>
         <Container>
-          <h1 className="text-8xl sm:text-9xl mb-20">
+          <h1 className="text-8xl sm:text-9xl my-20">
             <span className="text-oniViolet">Digital</span>&#8203;
             <span className="text-waveAqua2">Society</span>
           </h1>
 
           <Heading>
-            We build bespoke digital tools for complex requirements in web
-            applications, data engineering and cloud.
+            No requirement is too complex or ambitious. We build bespoke digital
+            tools with best practice and at pace.
           </Heading>
-
-          <div className="mb-12 text-lg sm:text-xl flex flex-col gap-4">
-            <p>
-              With our agile, sprint-based approach, engagements last as long as
-              you need and evolve with your priorities.
-            </p>
-            <p>
-              We are motivated by our mission to harness technology to improve
-              society and operate as a worker-owned not-for-profit co-operative.
-            </p>
-          </div>
+          <p className="text-end">
+            <Link href="">More about us {"\u2192"}</Link>
+          </p>
         </Container>
       </Section>
       <Section light>
         <Container>
-          <Heading anchor="projects">We get your projects off the ground</Heading>
-          <p className="text-base sm:text-lg">
-            With our bespoke application development experience, no requirement
-            is too complex or ambitious. We will bring your project to life with
-            best practice and at pace.
-          </p>
+          <div className="flex flex-col sm:flex-row justify-between gap-8 items-start">
+            <Heading anchor="projects">
+              Get your web, data and cloud projects off the ground
+            </Heading>
+            <a
+              data-umami-event="out-mail"
+              href="mailto:hello@digitalsociety.coop"
+              className="text-nowrap text-xl sm:text-2xl self-center p-3 rounded-xl border border-linkBlue hover:outline outline-linkBlue bg-linkBlue! hover:bg-waveBlue2! text-lighterYellow!"
+            >
+              Start your project with us
+            </a>
+          </div>
           <h3 className="text-lg sm:text-xl font-bold mt-6">
-            Some examples of our work
+            Featured projects
           </h3>
-          <div className="flex flex-col gap-10">
-            <Example
+          <div className="flex flex-col sm:flex-row sm:justify-start gap-8">
+            <Project
+              href="/projects/orang-energy/"
               title="Orang Energy"
-              href="https://orang.energy"
-              screenshots={[
-                {
-                  alt: "Screenshot from Orang Energy showing the calculator page",
-                  src: "/images/orang-energy-screenshot-1.png",
-                },
-                {
-                  alt: "Screenshot from Orang Energy showing the installer-directory",
-                  src: "/images/orang-energy-screenshot-2.png",
-                },
-              ]}
-              description="We developed the software for Orang Energy, an energy savings calculator, aimed at showing the savings achievable through sustainable technology in the home and connecting them with qualified local installers."
-              stack="Built using Next.js and Tailwind CSS. Deployed to DigitalOcean App Platform via Github Actions."
-              testimonial="Working in an agile way, Chris and Endre consistently met the aims of development sprints. Where the technical challenges arose or when late requirement changes were introduced, they communicated in a timely and proactive manner to solve any issues. Not only did they perform as an excellent software development team, they also showed great interest in the product design, and provided their own intelligent insights to help with the product design. I highly recommend Digital Society!"
+              description="Helping reduce your energy bills"
+              screenshots={
+                <img
+                  alt="Screenshot from Orang Energy showing the calculator page"
+                  src="/images/orang-energy-screenshot-1.png"
+                  className="w-full object-cover"
+                />
+              }
             />
-            <Example
+            <Project
+              href="/projects/dudley-editions/"
               title="Dudley Editions"
-              href="https://dudleyeditions.com"
-              screenshots={[
-                {
-                  alt: "Screenshot from Dudley Editions app showing the book library",
-                  src: "/images/dudley-editions-screenshot-2.jpg",
-                },
-                {
-                  alt: "Screenshot from Dudley Editions app showing a book description",
-                  src: "/images/dudley-editions-screenshot-1.jpg",
-                },
-                {
-                  alt: "Screenshot from Dudley Editions app showing my library",
-                  src: "/images/dudley-editions-screenshot-3.jpg",
-                },
-              ]}
-              description="We are the development partner for Dudley Editions, a mobile app to create and share personalised audiobooks in minutes."
-              stack="Built using Flutter and NestJS. Deployed to AWS via Github Actions and available on both iOS and Android app stores."
-              testimonial="The team at Digital Society are a delight to work with. They're always professional, approachable and flexible. They're committed to delivering high quality products and we couldn't be happier with the ongoing partnership we have them."
+              description="Creating connections through personalised audiobooks"
+              screenshots={
+                <>
+                  <img
+                    alt="Screenshot from Dudley Editions app showing the book library"
+                    src="/images/dudley-editions-screenshot-1.jpg"
+                    className="w-full object-cover"
+                  />
+                  <img
+                    alt="Screenshot from Dudley Editions app showing a book description"
+                    src="/images/dudley-editions-screenshot-2.jpg"
+                    className="w-full object-cover"
+                  />
+                  <img
+                    alt="Screenshot from Dudley Editions app showing my library"
+                    src="/images/dudley-editions-screenshot-3.jpg"
+                    className="w-full object-cover"
+                  />
+                </>
+              }
             />
           </div>
-          <a
-            data-umami-event="out-mail"
-            href="mailto:hello@digitalsociety.coop"
-            className="text-xl sm:text-2xl self-center my-4 p-3 rounded-xl border border-linkBlue hover:outline outline-linkBlue bg-linkBlue! hover:bg-waveBlue2! text-lighterYellow!"
-          >
-            Start your project with us
-          </a>
         </Container>
       </Section>
       <Section>
         <Container>
-          <Heading anchor="products">We help you realise the value of your data</Heading>
+          <Heading anchor="products">
+            We help you realise the value of your data
+          </Heading>
 
           <p className="text-base sm:text-lg">
             Poor fundamentals can limit the value you get from your data. We
@@ -147,7 +138,7 @@ export default function Home(): ReactNode {
                   src: "/images/example-healthdata.png",
                 },
               ]}
-              description={(
+              description={
                 <>
                   <Link href="https://epcdata.scot" target="_blank">
                     epcdata.scot
@@ -157,10 +148,10 @@ export default function Home(): ReactNode {
                     healthdata.scot
                   </Link>{" "}
                   are data portals that enable consuming datasets from
-                  applications, removing the toil and enabling innovation. Powered
-                  by tap.
+                  applications, removing the toil and enabling innovation.
+                  Powered by tap.
                 </>
-              )}
+              }
               stack="Built using Fresh. Deployed to Kubernetes via Github Actions."
             />
           </div>
@@ -228,22 +219,17 @@ function Example(props: {
 }): ReactNode {
   return (
     <div className="flex flex-col gap-3">
-      {props.href
-        ? (
-          <Link
-            href={props.href}
-            className="text-lg sm:text-xl font-bold mb-2"
-            target="_blank"
-          >
-            {props.title}
-          </Link>
-        )
-        : (
-          <h3 className="text-lg sm:text-xl font-bold mb-2">
-            {props.title}
-          </h3>
-        )
-      }
+      {props.href ? (
+        <Link
+          href={props.href}
+          className="text-lg sm:text-xl font-bold mb-2"
+          target="_blank"
+        >
+          {props.title}
+        </Link>
+      ) : (
+        <h3 className="text-lg sm:text-xl font-bold mb-2">{props.title}</h3>
+      )}
       <div className="flex flex-row flex-wrap gap-4 items-center">
         {props.screenshots.map(t => <Thumbnail {...t} />)}
       </div>
@@ -253,5 +239,27 @@ function Example(props: {
         <q className="italic">{props.testimonial}</q>
       )}
     </div>
+  );
+}
+
+function Project(props: {
+  href: string;
+  title: string;
+  description: string;
+  screenshots: React.ReactNode;
+}): React.ReactNode {
+  return (
+    <a
+      href={props.href}
+      className="flex-1 flex flex-col gap-4 p-4 rounded-xl bg-fujiWhite shadow items-start hover:underline"
+    >
+      <div className="max-h-[15rem] w-full self-center rounded-xl overflow-hidden aspect-320/213 flex gap-2 items-between">
+        {props.screenshots}
+      </div>
+      <h2 className="text-xl sm:text-2xl text-springBlue min-w-0">
+        <strong className="font-extrabold">{props.title}</strong>{" "}
+        <span className="font-light">-</span> {props.description}
+      </h2>
+    </a>
   );
 }

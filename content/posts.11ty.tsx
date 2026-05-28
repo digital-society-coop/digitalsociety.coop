4f1f8f1b8c93e9f87d10b2084ed2213162d5fae0
Endre Kadas <endre@digitalsociety.coop>
2026-05-28 11:36:21 +0100

    feat: Restyle posts

--- content/posts.11ty.tsx
+++ content/posts.11ty.tsx
@@ -3,7 +3,6 @@ import type { ReactNode } from "react";
 import Page from "../components/Page";
 import Section from "../components/Section";
 import PageTitle from "../components/PageTitle";
-import Heading from "../components/Heading";
 
 export default function Insights(): ReactNode {
   return (
@@ -13,7 +12,7 @@ export default function Insights(): ReactNode {
     >
       <Section>
         <PageTitle>Insights</PageTitle>
-        <div className="flex flex-col sm:flex-row flex-wrap sm:justify-start gap-4">
+        <div className="flex flex-col sm:flex-row flex-wrap sm:justify-start gap-8 sm:gap-16 sm:px-12">
           <Post
             href="/posts/pension-sovereignty/"
             title="UK pensions"
@@ -54,6 +53,7 @@ export default function Insights(): ReactNode {
             description="Our first data SaaS product"
             alt="Logo for tap"
             src="/images/tap.svg"
+            imgContainerClass="bg-sumiInk2 p-8"
           />
           <Post
             href="/posts/first-year/"
@@ -81,24 +81,17 @@ function Post(props: {
   return (
     <a
       href={props.href}
-      className="flex-1 flex flex-col p-4 rounded-xl bg-fujiWhite shadow items-start hover:underline min-w-[calc((100%-2*4*var(--spacing))/3)] max-w-full"
+      className="flex-1 flex flex-col rounded-xl bg-lighterYellow shadow items-start hover:underline min-w-[calc((100%-2*4*var(--spacing))/4)] max-w-full"
     >
       <div
-        className={`max-h-[15rem] max-w-[20rem] w-full self-center rounded-xl overflow-hidden bg-sumiInk1 aspect-320/213 flex flex-col justify-center ${props.imgContainerClass}`}
+        className={`max-h-[15rem] w-full self-center overflow-hidden aspect-320/213 flex gap-2 items-between rounded-t-xl ${props.imgContainerClass ?? ""}`}
       >
-        <img alt={props.alt} src={props.src} className="w-full" />
+        <img alt={props.alt} src={props.src} className="w-full object-cover" />
       </div>
-      <div className="flex-1 flex flex-col justify-between text-springBlue min-w-0 overflow-hidden w-full">
-        <Heading>{props.title}</Heading>
-        <div className="flex flex-col gap-2 min-w-0 overflow-hidden w-full">
-          <p>{props.date}</p>
-          <p
-            className="overflow-hidden text-ellipsis sm:text-nowrap"
-            title={props.description}
-          >
-            {props.description}
-          </p>
-        </div>
+      <div className="text-sumiInk2 min-w-0 p-4 text-center flex flex-col gap-2 justify-between self-center">
+        <h2 className="text-xl sm:text-2xl font-extrabold">{props.title}</h2>
+        <p className="text-lg sm:text-xl">{props.date}</p>
+        <p className="text-lg sm:text-xl">{props.description}</p>
       </div>
     </a>
   );
